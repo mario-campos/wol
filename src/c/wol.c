d49e3dfdb61e73c6bdb78209f33f097d68746aae
Mario Campos <mcampos@mail.bradley.edu>
2012-07-19 00:21:14 -0500

    finally fixed the passwd bug. ether_aton() overrides the buffer with every subsequent call. still not completely working though

--- src/c/wol.c
+++ src/c/wol.c
@@ -6,12 +6,14 @@
 ** Copyright 2012 Mario Campos
 */
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <errno.h>
 #include <sys/types.h>
-#include <sys/socket.h>                        /* socket */
+#include <stdio.h>                             /* perror(), puts() */
+#include <string.h>                            /* memcpy() */
+#include <unistd.h>                            /* close() */
+#include <arpa/inet.h>                         /* htons() */
+#include <sys/socket.h>                        /* socket() */
 #include <linux/if_packet.h>                   /* sockaddr_ll */
 #include <linux/if_ether.h>                    /* ETH_ALEN */
 #include <net/ethernet.h>                      /* struct ether_addr */
@@ -38,28 +40,32 @@ int aindex(char *str, char **array, unsigned int arraylen)
 
 void printHelp()
 {
-  puts("wol [--help|-h] [--quiet|-q] [--version|-v] [--interface|-i <name>] [--password|-p <passwd>] <mac address>\n");
-  puts("--help|-h\t\tPrints this help message and exit.");
-  puts("--quiet|-q\t\tDisable output.");
-  puts("--version|-v\t\tPrints version number and exit.");
-  puts("--interface|-i <name>\tSpecify the interface for the packet (default eth0).");
-  puts("--password|-p <passwd>\tSend a colon-delimited hex password (default none).");
-  puts("<mac address>\t\tMAC-48 address (colon delimited).");
+  puts("wol [options] {mac address}\n");
+  puts("[--help|-h]\t\t   Prints this help message and exit.");
+  puts("[--quiet|-q]\t\t   Disable output.");
+  puts("[--version|-v]\t\t   Prints version number and exit.");
+  puts("[--interface|-i {name}]\t   Specify the interface for the packet (default eth0).");
+  puts("[--password|-p {passwd}]   Send a colon-delimited hex password (default none).");
 }
 
-int main(unsigned int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int index;
   unsigned char i;
-  int buflen = WOL_DATA_LEN;                   /* buffer length */
-  int sockfd;                                  /* socket file descripter */
-  unsigned char iface_index;                   /* interface index number */
-  unsigned char use_passwd = FALSE;            /* assume no password */
-  unsigned char quiet = FALSE;                 /* assume verbose output */
-  void *buf, *payload;                         /* Buffer pointers */
-  struct sockaddr_ll dest_addr;                /* ethernet frame dest address */
-  struct ether_addr *wol_addr;                 /* wol-target mac address */
-  struct ether_addr *password;                 /* wol password in mac-48 format */
+  int buflen = WOL_DATA_LEN;                        /* buffer length */
+  int sockfd;                                       /* socket file descripter */
+  unsigned int iface_index;                         /* interface index number */
+  unsigned int use_passwd = FALSE;                  /* assume no password */
+  unsigned int quiet = FALSE;                       /* assume verbose output */
+  void *buf, *payload;                              /* Buffer pointers */
+  struct sockaddr_ll dest_addr;                     /* ethernet frame dest address */
+  struct ether_addr *mac_addr, wol_addr, password;  /* mac address structure (6 bytes structs) */
+
+  /* no command-line arguments */
+  if(argc == 1) {
+    printHelp();
+    return EXIT_SUCC;
+  }
 
   /* parse command-line for switches */
   if(aindex("-h", argv, argc) != NO_INDEX || aindex("--help", argv, argc) != NO_INDEX) {
@@ -87,19 +93,21 @@ int main(unsigned int argc, char *argv[])
      (index = aindex("--password", argv, argc)) != NO_INDEX ||
      (index = aindex("-p", argv, argc))         != NO_INDEX
      ) {
-    if((password = ether_aton(argv[index+1])) == NULL) {
+    if((mac_addr = ether_aton(argv[index+1])) == NULL) {
       fprintf(stderr, "error: invalid password; follow password format\n");
       return EXIT_ERR;
     }
     buflen = WOL_DATA_LEN + WOL_PASSWD_LEN;
     use_passwd = TRUE;
+    memcpy(&password, mac_addr, WOL_PASSWD_LEN);
   }
 
   /* input validation and serialization */
-  if((wol_addr = ether_aton(argv[argc-1])) == NULL) {
+  if((mac_addr = ether_aton(argv[argc-1])) == NULL) {
     fprintf(stderr, "error: target isn't a MAC-48 address\n");
     return EXIT_ERR;
   }
+  memcpy(&wol_addr, mac_addr, WOL_PASSWD_LEN);
 
   /* create "packet" (ethernet frame) socket */
   if((sockfd = socket(PF_PACKET, SOCK_DGRAM, htons(ETH_P_WOL))) == -1) {
@@ -114,9 +122,9 @@ int main(unsigned int argc, char *argv[])
   /* prepare frame payload */
   memset(payload, 0xFF, ETH_ALEN);
   for(i=0, payload += ETH_ALEN; i < 16; ++i, payload += ETH_ALEN)
-    memcpy(payload, wol_addr, ETH_ALEN);
+    memcpy(payload, &wol_addr, ETH_ALEN);
   if(use_passwd)
-    memcpy(payload, password, WOL_PASSWD_LEN);
+    memcpy(payload, &password, WOL_PASSWD_LEN);
 
   /* prepare socket destination struct */
   dest_addr.sll_family = AF_PACKET;
@@ -135,8 +143,8 @@ int main(unsigned int argc, char *argv[])
 
   if(quiet != TRUE) 
     printf("Magic Packet sent to %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n",
-	   wol_addr->ether_addr_octet[0], wol_addr->ether_addr_octet[1], wol_addr->ether_addr_octet[2],
-	   wol_addr->ether_addr_octet[3], wol_addr->ether_addr_octet[4], wol_addr->ether_addr_octet[5]);
+	   wol_addr.ether_addr_octet[0], wol_addr.ether_addr_octet[1], wol_addr.ether_addr_octet[2],
+	   wol_addr.ether_addr_octet[3], wol_addr.ether_addr_octet[4], wol_addr.ether_addr_octet[5]);
 
   /* clean up */
   free(buf);
