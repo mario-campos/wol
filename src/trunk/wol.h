440aec26858550cdd41ba980e67df128d800da68
Mario Campos <mcampos@mail.bradley.edu>
2012-07-22 12:40:16 -0500

    fix typo
    removed 'zz' as it was a typo and would have caused syntactic errors or unexpected behavior

--- src/trunk/wol.h
+++ src/trunk/wol.h
@@ -6,4 +6,4 @@
 
 #define WOL_DATA_LEN       102      /* Max Length of a Wake-On-LAN packet */
 #define WOL_PASSWD_LEN     6        /* Max Length of a Wake-On-LAN password */
-#define ETH_P_WOL          0x0842   zz/* Ethernet Protocol ID for Wake-On-LAN */
+#define ETH_P_WOL          0x0842   /* Ethernet Protocol ID for Wake-On-LAN */
